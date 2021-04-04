1efba704c1e6c731194ba7f03f62a085885fe9cc
Tiago Gomes <tigomes.santos@gmail.com>
2021-04-04 12:36:23 -0300

    Versão Inicial

---
+++ cypress/integration/arrow.spec.js
@@ -0,0 +1,22 @@
+const { it } = require("mocha");
+
+it('nada agora', function () { })
+    function soma(a, b) {
+       return a + b;
+    }
+
+    //const soma = (a, b) => {
+      //  return a+b
+    //}
+
+    //const soma=(a,b) => a+b
+
+    //const soma=(a) =>  a+a
+
+    //const soma=a=>a+a
+
+    //const soma=()=> 5+5
+
+    console.log(soma(1,4))
+
+    
