891d0d44d19079a469bdf18e3680850b328d406a
Tiago Gomes <tigomes.santos@gmail.com>
2021-04-24 14:09:48 -0300

    Enter

--- cypress/integration/promisses.spec.js
+++ cypress/integration/promisses.spec.js
@@ -1,11 +1,26 @@
-it('sem testes ainda',()={})
 
-const getSomething = () => 10;
+it('sem testes, ainda', () => { })
+
+//const getSomething = () => 10;
+
+const getSomething = callback => {
+    setTimeout(()=>{
+        //console.log('respondendo...')
+        callback(12);
+    },1000)
+
+}
 
 const system = () =>{
     console.log('init');
-    const something = getSomething();
-    console.log('Something is ${something}');
-    
+    //const something = getSomething ();
+    //getSomething(some => console.log('Something is ${some}'));
+    getSomething(some => {
+        console.log('Something is ${some}')
+    //console.log('Something is ${something}');
+    console.log('end');
+})
 
 }
+
+system();
