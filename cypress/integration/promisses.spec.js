ae137980f55ff556ecd0471e856ec7e8b13ce9b8
Tiago Gomes <tigomes.santos@gmail.com>
2021-04-24 15:21:34 -0300

    Enter

--- cypress/integration/promisses.spec.js
+++ cypress/integration/promisses.spec.js
@@ -1,5 +1,3 @@
-import { reject } from "cypress/types/bluebird";
-
 it('sem testes, ainda', () => { })
 
 //const getSomething = () => 10;
@@ -9,20 +7,27 @@ const getSomething = () => {
     return new Promise((resolve, reject)=>{
         setTimeout(()=>{
         //console.log('respondendo...')
-        callback(13);
-    },1000)
+        resolve(13);
+        },1000)
 })
+
 }
 
-const system = async () =>{
+const system = () =>{
     console.log('init');
     //const something = getSomething ();
     //getSomething(some => console.log('Something is ${some}'));
     //getSomething(some => {
-    const some = await getSomething()
-    console.log('Something is ${some}')
+    //const some = await getSomething()
+    //console.log('Something is ${some}')
     //console.log('Something is ${something}');
-    console.log('end');
+    getSomething().then( some => {
+        console.log('Something is ${some}')
+        console.log('end');
+
+    })
+
 }
 
 system();
+
