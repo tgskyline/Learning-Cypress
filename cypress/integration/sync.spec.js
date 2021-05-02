5d3266ae483df1029aa7781b0d8d4d560b723a00
tgskyline <tigomes.santos@gmail.com>
2021-05-02 17:17:12 -0300

    Enter

--- cypress/integration/sync.spec.js
+++ cypress/integration/sync.spec.js
@@ -19,4 +19,46 @@ describe('Esperas...',()=>{
         cy.get('#novoCampo').type('funciona')
     })
 
+    it('Deve fazer retrys',() => {
+
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#buttonDelay').click()
+        cy.get('#novoCampo').should('not.exist')
+        cy.get('#novoCampo')
+            .should('exist')
+            .type('funciona')
+
+// Não se pode fazer assertivas que uma seja oposto da outra!
+    })
+
+        it('Uso do find',()=>{
+
+//Atenção ao usar um "find" porque le fica preso no primeiro ponto usado
+            
+            cy.get('#buttonList').click()
+            cy.get('#lista li')
+                .find('span')
+                .should('contain', 'Item 1')
+            //cy.get('#lista li')
+              //  .find('span')
+                //.should('contain', 'Item 2')
+            cy.get('#lista li span')
+                .should('contain', 'Item 2')
+        })
+
+        it.only('Uso do find listacom DOM',()=>{
+          
+            cy.get('#buttonListDOM').click()
+            cy.get('#lista li')
+                .find('span')
+                .should('contain', 'Item 1')
+            //cy.get('#lista li')
+              //  .find('span')
+                //.should('contain', 'Item 2')
+            cy.get('#lista li span')
+               .should('contain', 'Item 2')
+        
+        })
+
+
 })
