0759021c95ad50e2e5b5bb5c0ecfa27cd6f55384
tgskyline <tigomes.santos@gmail.com>
2021-05-22 21:12:02 -0300

    Helpers

--- cypress/integration/helpers.spec.js
+++ cypress/integration/helpers.spec.js
@@ -26,5 +26,21 @@ describe('Helpers...',()=>{
         cy.wrap(1).then(num =>{
             return 2
         }).should('be.equal',2)
+           
     })
+
+    it.only('Its...', ()=>{
+        const obj = {nome:'User', idade:20}
+        cy.wrap(obj).should('have.property', 'nome','User')
+        cy.wrap(obj).its('nome').should('be.equal', 'User')
+
+        const obj2 ={nome: 'User', idade: 20, endereco: {rua: 'dos dos bolos'}}
+        cy.wrap(obj2).its('endereco').should('have.property', 'rua')
+        cy.wrap(obj2).its('endereco').its('rua').should('contain','bolos')
+        cy.wrap(obj2).its('endereco.rua').should('contain','bolos')
+
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.title().its('length').should('be.equal',20)
+    })
+
 })
