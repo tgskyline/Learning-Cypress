12242e9cb83862981d60e17870f7b95986edfdc2
tgskyline <tigomes.santos@gmail.com>
2021-06-13 14:12:40 -0300

    iFrame

---
+++ cypress/integration/iframe.spec.js
@@ -0,0 +1,22 @@
+/// <reference types = "cypress" />
+
+describe('Work with Alerts', () => {
+    it('Deve preencher campo de texto',()=>{
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+        cy.viewport(1000,700)
+        cy.get('#frame1').then(iframe=>{
+            const body = iframe.contents().find('body')
+            cy.wrap(body).find('#tfield')
+                .type('Carambola')
+                .should('have.value','Carambola')
+        
+        cy.on('window:alert', msg =>{
+            expect(msg).to.be.equal('Alert Simples')
+        })
+        cy.wrap(body).find('#otherButton').click()   
+    
+        })
+    
+    })    
+ 
+})
