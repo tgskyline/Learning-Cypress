533b2117fe94cd93fda28fc38fe5fd0804790240
tgskyline <tigomes.santos@gmail.com>
2021-06-13 12:47:59 -0300

    Desafio

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -32,7 +32,7 @@ describe('Work with Alerts', () => {
          
     })
     
-    it.only('Confirm',()=>{
+    it('Confirm',()=>{
         cy.on('window:confirm', msg => {
             expect(msg).to.be.equal('Confirm Simples')
         })
@@ -41,6 +41,50 @@ describe('Work with Alerts', () => {
         })
         cy.get('#confirm').click()
         
-    })   
-})
+    })
     
+    it('Deny',()=>{
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+            return false
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Negado')
+        })
+        cy.get('#confirm').click()
+        
+    })
+
+    it('Prompt',()=>{
+        cy.window().then(win=>{
+            cy.stub(win,'prompt').returns('42')
+        })
+        cy.on('window:confirm', msg => {
+          expect(msg).to.be.equal('Era 42?')
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal(':D')
+        })
+        cy.get('#prompt').click()
+        
+    })
+    it.only('validando Mensagens',()=>{
+        const stub = cy.stub().as('alerta')
+        cy.on('window:alert',stub)
+        cy.get('#formCadastrar').click()
+            .then(()=> expect (stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio'))
+            
+        cy.get('#formNome').type('Tiago')
+        cy.get('#formCadastrar').click()
+            .then(()=> expect (stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio'))
+       
+        cy.get('[data-cy=dataSobrenome]').type('Gomes dos Santos')
+        cy.get('#formCadastrar').click()
+            .then(()=> expect (stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio'))    
+
+        cy.get('#formSexoMasc').click()
+        cy.get('#formCadastrar').click()
+
+        cy.get('#resultado > :nth-child(1)').should('contain','Cadastrado!')
+    })
+})
