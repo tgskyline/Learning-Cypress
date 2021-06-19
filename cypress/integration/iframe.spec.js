7a97eabac49869d8b1f929524937402e182e00b7
tgskyline <tigomes.santos@gmail.com>
2021-06-19 09:22:51 -0300

    Frame Diretamente

--- cypress/integration/iframe.spec.js
+++ cypress/integration/iframe.spec.js
@@ -13,10 +13,17 @@ describe('Work with Alerts', () => {
         cy.on('window:alert', msg =>{
             expect(msg).to.be.equal('Alert Simples')
         })
-        cy.wrap(body).find('#otherButton').click()   
-    
-        })
-    
-    })    
- 
+        //cy.wrap(body).find('#otherButton').click()   
+    })
 })
+
+    it('Deve testar frame diretamente',()=>{
+        cy.visit('https://wcaquino.me/cypress/frame.html')        
+        cy.get('#otherButton').click()   
+        cy.on('window:alert', msg =>{
+            expect(msg).to.be.equal('Click OK!')
+        })  
+    })    
+})
+
+
