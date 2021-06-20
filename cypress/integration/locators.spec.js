37ac2ed4e1888d6097f100ff9df272e4eaf9aad6
tgskyline <tigomes.santos@gmail.com>
2021-06-20 16:09:38 -0300

    Locators

--- cypress/integration/locators.spec.js
+++ cypress/integration/locators.spec.js
@@ -18,9 +18,11 @@ describe('Work with basic elemets', () => {
     //     selectorPriority: ['id', 'class', 'attributes'],
     //   })
 
-    it('...', () =>{
+    // Página para consulta de JQuery Selector https://www.w3schools.com/jquery/jquery_ref_selectors.asp
 
-
+    it('using jquery selector', () =>{
+        cy.get(':nth-child(1) > :nth-child(3) > [type="button"]')
+        cy.get('table#tabelaUsuarios tbody >tr:eq(0) td:nth-child(3) >input').click()
     })
 
 })    
